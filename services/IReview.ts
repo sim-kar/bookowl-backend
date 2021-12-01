ef3ab4c46582019307f88c9e54a1e85b61692163
sika2001 <sika2001@student.miun.se>
2021-12-01 19:34:08 +0100

    added interfaces for book, review, user

---
+++ services/IReview.ts
@@ -0,0 +1,7 @@
+export default interface IReview {
+  isbn: string;
+  username: string;
+  stars: number;
+  text: string;
+  date: string;
+}
---
+++ services/IUser.ts
@@ -0,0 +1,6 @@
+export default interface IUser {
+  username: string;
+  email: string;
+  password: string;
+  joined: string;
+}
