d40677589ecf7d115921a61bb1cd110dd91938b5
sika2001 <sika2001@student.miun.se>
2021-12-21 12:08:02 +0100

    updated constants

--- utils/Constants.ts
+++ utils/Constants.ts
@@ -5,13 +5,13 @@ const Constants = {
   // https://html.spec.whatwg.org/multipage/input.html#valid-e-mail-address
   EMAIL_REGEX: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
   // TODO: filepath to placeholder image for books without a thumbnail from public API
-  PLACEHOLDER_IMAGE: '',
+  PLACEHOLDER_IMAGE: '../../assets/images/placeholder_cover.jpg',
   // field used to search by title in public api
   TITLE_FIELD: 'intitle',
   // field used to search by author in public api
   AUTHOR_FIELD: 'inauthor',
   // maximum number of results to get when searching public api
-  MAX_RESULTS: 10,
+  MAX_RESULTS: 15,
 };
 
 export default Object.freeze(Constants);
