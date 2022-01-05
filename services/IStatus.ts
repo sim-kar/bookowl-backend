23ca2206084371311995f196fa8d04b5c9ba2b6a
sika2001 <sika2001@student.miun.se>
2022-01-05 20:58:22 +0100

    updated JSDoc

--- services/IStatus.ts
+++ services/IStatus.ts
@@ -2,9 +2,9 @@ import mongoose from 'mongoose';
 import IBook from './IBook';
 
 /**
- * A user status for a book, i.e. whether the user has read the book, is reading it, or wants to
- * read it. Contains a reference to the book.
- * */
+ * A user status for a book, i.e. whether the user has read the book (2), is reading it (1),
+ * or wants to read it (0). Contains a reference to the book.
+ */
 export default interface IStatus extends mongoose.Document {
   isbn: string;
   username: string;
